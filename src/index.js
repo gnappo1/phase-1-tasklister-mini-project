2cfa3bb5d0f63b087805c875f5081d16cf8be903
Matteo Piccini <gnappo.1@live.it>
2023-04-14 15:41:43 -0700

    after-walkthrough code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+const form = document.querySelector("form") // checks and stores the dom node right away and keeps a memory of it FOREVER
+//! const form = () => document.querySelector("form") // checks for the presence of the element at the time of invocation
+
+const createTask = (taskValue, selectedPriority) => {
+  const li = document.createElement("li")
+  li.classList.add("task-item", selectedPriority) 
+  li.innerText = taskValue
+
+  
+  const button = document.createElement("button")
+  button.className = "delete-btn"
+  button.innerText = "X"
+  button.addEventListener("click", () => li.remove())
+
+  li.appendChild(button)
+  tasks.appendChild(li)
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault() // no page refreshes
+  // debugger
+  let userInput = e.target["new-task-description"].value
+  const priorityLevel = e.target.priority.value
+  debugger
+  if (!!userInput.trim()) {
+    createTask(userInput, priorityLevel)
+    e.target.reset()
+  } else {
+    alert("You must fill out both inputs")
+  }
+  // e.target["new-task-description"].value = "" // empty input manually
+})
+// });
