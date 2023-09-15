910c37675a7ab962fada2faf5ca99d26b5914d66
Matteo Piccini <gnappo.1@live.it>
2023-09-15 15:34:28 -0700

    end of walkthrough code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,47 @@
+
 // document.addEventListener("DOMContentLoaded", () => {
-const form = document.querySelector("form") // checks and stores the dom node right away and keeps a memory of it FOREVER
-//! const form = () => document.querySelector("form") // checks for the presence of the element at the time of invocation
-
-const createTask = (taskValue, selectedPriority) => {
-  const li = document.createElement("li")
-  li.classList.add("task-item", selectedPriority) 
-  li.innerText = taskValue
-
   
-  const button = document.createElement("button")
-  button.className = "delete-btn"
-  button.innerText = "X"
-  button.addEventListener("click", () => li.remove())
+// });
 
-  li.appendChild(button)
-  tasks.appendChild(li)
+//! CREATE GLOBAL VARIABLES
+const taskForm = document.querySelector("#create-task-form")
+const taskUl = document.querySelector("#tasks")
+
+function addTask(e) {
+  //! block page refreshes
+  e.preventDefault()
+  //! determine what the user typed
+  const input = e.target["new-task-description"].value
+  //! Create new li with user input
+  const newLi = document.createElement('li') //* returns a new Node
+  newLi.textContent = input
+  //! Create a new button with X as text
+  const btn = document.createElement("button")
+  btn.addEventListener("click", () => newLi.remove() )
+  btn.textContent = "X"
+  //! determine which priority the user selected
+  const priority = e.target.priority.value // "low", "medium", "high"
+
+  // if (priority === "low") {
+  //   newLi.style.backgroundColor = "green"
+  // } else if (priority === "medium") {
+  //   newLi.style.backgroundColor = "yellow"
+  // } else {
+  //   newLi.style.backgroundColor = "red"
+  // }
+  newLi.classList.add(priority)
+
+  //! append button within the li
+  newLi.appendChild(btn)
+  //! Append the li as child of ul
+  taskUl.appendChild(newLi)
+  //! Empty the form
+  e.target.reset()
 }
 
-form.addEventListener('submit', e => {
-  e.preventDefault() // no page refreshes
-  // debugger
-  let userInput = e.target["new-task-description"].value
-  const priorityLevel = e.target.priority.value
-  debugger
-  if (!!userInput.trim()) {
-    createTask(userInput, priorityLevel)
-    e.target.reset()
-  } else {
-    alert("You must fill out both inputs")
-  }
-  // e.target["new-task-description"].value = "" // empty input manually
-})
-// });
+function attachListeners() {
+  //! attach all listeners that we might need
+  taskForm.addEventListener("submit", addTask)
+}
+
+attachListeners()
